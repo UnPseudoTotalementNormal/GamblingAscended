230fb2546bf0a144269e54ae26813f39872c6ef7
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2024-11-17 23:39:14 +0100

    Adding game event cleaner to clean listeners

--- Assets/Scripts/GameEvents/GameEventCleaner.cs
+++ Assets/Scripts/GameEvents/GameEventCleaner.cs
@@ -1,13 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using GameEvents;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class GameEventCleaner : MonoBehaviour
 {
-    [SerializeField] private GameEvent _gameEvent;
+    private List<IGameEvent> _gameEvents;
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _ = LoadGameEvents();
+    }
+
+    private async Task LoadGameEvents()
+    {
+        AsyncOperationHandle<IList<IGameEvent>> handle = Addressables.LoadAssetsAsync<IGameEvent>("GameEvent", null);
+        await handle.Task;
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            _gameEvents = handle.Result.ToList();
+        }
+    }
+    
     private void OnDestroy()
     {
-        _gameEvent.ClearListeners();
+        foreach (IGameEvent gameEvent in _gameEvents)
+        {
+            gameEvent.ClearListeners();
+        }
     }
 }
