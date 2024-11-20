511129b30eeef2c79817f3069ea0761283ef8a94
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2024-11-20 21:16:34 +0100

    Adding characterMovement

---
+++ Assets/Scripts/Character/GL_CharacterMovement.cs
@@ -0,0 +1,93 @@
+using System;
+using GameEvents;
+using UnityEngine;
+
+namespace Character
+{
+    public class GL_CharacterMovement : MonoBehaviour
+    {
+        private Transform _transform;
+        private Rigidbody _rigidbody;
+        
+        [SerializeField] private GameEvent<GameEventInfo> _moveInputEvent;
+
+        [Header("Locomotion")]
+        [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _maxAccelForce;
+
+        [Header("Friction")] 
+        [SerializeField] private Vector3 _frictionForce;
+        
+        private Vector2 _direction;
+        
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+            _rigidbody = GetComponent<Rigidbody>();
+            
+            _moveInputEvent.AddListener(OnMoveInput);
+        }
+
+        private void OnMoveInput(GameEventInfo eventInfo)
+        {
+            if (!eventInfo.TryTo(out GameEventVector2 gameEventVector2))
+            {
+                return;
+            }
+
+            SetDirection(gameEventVector2.Value);
+        }
+        
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        private void FixedUpdate()
+        {
+            ApplyFriction();
+            MoveCharacter(_direction);
+        }
+
+        private void ApplyFriction()
+        {
+            Vector3 frictionForce = Vector3.zero;
+            frictionForce.Set(
+                _frictionForce.x * -_rigidbody.linearVelocity.x, 
+                _frictionForce.y * -_rigidbody.linearVelocity.y, 
+                _frictionForce.z * -_rigidbody.linearVelocity.z);
+            _rigidbody.AddForce(frictionForce, ForceMode.Acceleration);
+        }
+
+        private void MoveCharacter(Vector2 direction)
+        {
+            if (direction == Vector2.zero)
+            {
+                return;
+            }
+    
+            Vector3 moveDirection = new Vector3(direction.x, 0, direction.y);
+            Vector3 currentVelocity = _rigidbody.linearVelocity;
+            if (moveDirection.magnitude > 1)
+            {
+                moveDirection.Normalize();
+            }
+            moveDirection = _transform.TransformDirection(moveDirection);
+            
+            
+            Vector3 acceleration = moveDirection * _acceleration;
+            if (acceleration.magnitude > _maxAccelForce)
+            {
+                acceleration = acceleration.normalized * _maxAccelForce;
+            }
+            
+            float accelDot = Vector3.Dot(moveDirection, currentVelocity.normalized);
+            float maxAccel = Mathf.Min((_maxSpeed - (currentVelocity.magnitude * accelDot)) / Time.fixedDeltaTime, _maxAccelForce);
+            maxAccel = Mathf.Max(maxAccel, 0);
+            acceleration = Vector3.ClampMagnitude(acceleration, maxAccel);
+            
+            _rigidbody.AddForce(acceleration, ForceMode.Acceleration);
+        }
+    }
+}
