ca8a2ce8d51f8a43df34be889e1ed23f6c8db967
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-11-24 22:40:39 +0100

    Adding placeholder switch time of day

--- Assets/Scripts/TimeSystem/GL_TimeManager.cs
+++ Assets/Scripts/TimeSystem/GL_TimeManager.cs
@@ -1,14 +1,29 @@
-using System;
-using Unity.Mathematics;
+using DG.Tweening;
+using GameEvents;
 using UnityEngine;
 
-[ExecuteAlways]
 public class GL_TimeManager : MonoBehaviour
 {
     [SerializeField] private Light _directionalLight;
     [SerializeField] private GL_LightingPreset _lightingPreset;
     
     [SerializeField, Range(0f, 24f)] private float _timeOfDay;
+
+    [SerializeField] private GameEvent<GameEventInfo> _switchTimeOfDay;
+
+    private const float DAY_TIME = 12;
+    private const float NIGHT_TIME = 24;
+    
+    private enum StateOfDay
+    {
+        Day,
+        Night,
+    }
+
+    private void Awake()
+    {
+        _switchTimeOfDay?.AddListener(SwitchTimeOfDay);
+    }
 
     private void Update()
     {
@@ -19,10 +34,36 @@ public class GL_TimeManager : MonoBehaviour
 
         //_timeOfDay += Time.deltaTime;
         //_timeOfDay %= 24;
-        
+
         UpdateLighting(_timeOfDay/24f);
     }
 
+    private void SwitchTimeOfDay(GameEventInfo eventInfo)
+    {
+        switch (GetStateOfDay())
+        {
+            case StateOfDay.Day:
+                DOTween.To(() => _timeOfDay, x => _timeOfDay = x, NIGHT_TIME, 3)
+                    .SetEase(Ease.Linear);
+                break;
+            case StateOfDay.Night:
+                _timeOfDay = 0;
+                DOTween.To(() => _timeOfDay, x => _timeOfDay = x, DAY_TIME + 2, 3)
+                    .SetEase(Ease.Linear);
+                break;
+        }
+    }
+
+    private StateOfDay GetStateOfDay()
+    {
+        if (_timeOfDay is < DAY_TIME + DAY_TIME/2f and > DAY_TIME - DAY_TIME/2f)
+        {
+            return StateOfDay.Day;
+        }
+        
+        return StateOfDay.Night;
+    }
+    
     private void UpdateLighting(float timePercent)
     {
         RenderSettings.ambientLight = _lightingPreset.AmbiantColor.Evaluate(timePercent);
@@ -37,6 +78,7 @@ public class GL_TimeManager : MonoBehaviour
 
     private void OnValidate()
     {
+        UpdateLighting(_timeOfDay/24f);
         if (_directionalLight)
         {
             return;
