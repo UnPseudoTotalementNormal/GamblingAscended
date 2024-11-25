308e98e1f9ba2b96be06003c47da89b5ba40d80e
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-11-25 17:28:26 +0100

    Switching time of day on wave event

--- Assets/Scripts/TimeSystem/GL_TimeManager.cs
+++ Assets/Scripts/TimeSystem/GL_TimeManager.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using GameEvents;
+using GameEvents.Enum;
 using UnityEngine;
 
 public class GL_TimeManager : MonoBehaviour
@@ -11,18 +12,25 @@ public class GL_TimeManager : MonoBehaviour
 
     [SerializeField] private GameEvent<GameEventInfo> _switchTimeOfDay;
 
-    private const float DAY_TIME = 12;
+    private const float DAY_TIME = 14;
     private const float NIGHT_TIME = 24;
     
-    private enum StateOfDay
-    {
-        Day,
-        Night,
-    }
-
     private void Awake()
     {
-        _switchTimeOfDay?.AddListener(SwitchTimeOfDay);
+        GameEventEnum.OnWaveStarted.AddListener(TimeSetDay);
+        GameEventEnum.OnWaveEnded.AddListener(TimeSetNight);
+    }
+
+    private void TimeSetNight(GameEventInfo eventInfo)
+    {
+        DOTween.To(() => _timeOfDay, x => _timeOfDay = x, NIGHT_TIME, 20)
+            .SetEase(Ease.Linear);
+    }
+
+    private void TimeSetDay(GameEventInfo eventInfo)
+    {
+        DOTween.To(() => _timeOfDay, x => _timeOfDay = x, DAY_TIME, 20)
+            .SetEase(Ease.Linear);
     }
 
     private void Update()
@@ -36,32 +44,6 @@ public class GL_TimeManager : MonoBehaviour
         //_timeOfDay %= 24;
 
         UpdateLighting(_timeOfDay/24f);
-    }
-
-    private void SwitchTimeOfDay(GameEventInfo eventInfo)
-    {
-        switch (GetStateOfDay())
-        {
-            case StateOfDay.Day:
-                DOTween.To(() => _timeOfDay, x => _timeOfDay = x, NIGHT_TIME, 3)
-                    .SetEase(Ease.Linear);
-                break;
-            case StateOfDay.Night:
-                _timeOfDay = 0;
-                DOTween.To(() => _timeOfDay, x => _timeOfDay = x, DAY_TIME + 2, 3)
-                    .SetEase(Ease.Linear);
-                break;
-        }
-    }
-
-    private StateOfDay GetStateOfDay()
-    {
-        if (_timeOfDay is < DAY_TIME + DAY_TIME/2f and > DAY_TIME - DAY_TIME/2f)
-        {
-            return StateOfDay.Day;
-        }
-        
-        return StateOfDay.Night;
     }
     
     private void UpdateLighting(float timePercent)
