fb96c2890476829170973f1bb4292160cc1791fd
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-11-28 10:41:33 +0100

    Dropping purse on enemy death

--- Assets/Scripts/Character/GL_MoneyPurse.cs
+++ Assets/Scripts/Character/GL_MoneyPurse.cs
@@ -1,13 +1,17 @@
 using System;
 using GameEvents;
+using GameEvents.Enum;
 using UnityEngine;
 
 public class GL_MoneyPurse : MonoBehaviour, GL_ICoinHolder
 {
     public float MoneyInserted { get; private set; }
+    public bool DropPurseOnDeath = false;
+    [SerializeField] private GameObject _pursePrefab;
 
     private void Awake()
     {
+        GameEventEnum.OnDeath.AddListener(OnDeath);
         AddMoney(15);
     }
     
@@ -21,5 +25,15 @@ public class GL_MoneyPurse : MonoBehaviour, GL_ICoinHolder
     {
         MoneyInserted -= value;
         MoneyInserted = Math.Max(0, MoneyInserted);
+    }
+
+    private void OnDeath(GameEventInfo eventInfo)
+    {
+        if (!gameObject.HasGameID(eventInfo.Ids) || !DropPurseOnDeath)
+        {
+            return;
+        }
+        
+        Instantiate(_pursePrefab, transform.position, Quaternion.identity).GetComponent<GL_ICoinHolder>().AddMoney(MoneyInserted);
     }
 }
