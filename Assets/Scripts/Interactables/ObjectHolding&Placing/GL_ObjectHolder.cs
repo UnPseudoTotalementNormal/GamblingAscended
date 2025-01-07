b81a929f8b360f38fd67d93cad548e2fe78bc892
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-01-08 00:22:41 +0100

    Better lighting

--- Assets/Scripts/Interactables/ObjectHolding&Placing/GL_ObjectHolder.cs
+++ Assets/Scripts/Interactables/ObjectHolding&Placing/GL_ObjectHolder.cs
@@ -97,7 +97,6 @@ public class GL_ObjectHolder : MonoBehaviour
 
     private void RotatePlaceable()
     {
-        Debug.Log("cool");
         _placeRotation.y += _rotationSpeed * Time.deltaTime;
     }
     
